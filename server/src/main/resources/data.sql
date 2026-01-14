d2ad6c261a2aba34fc0cda7e70c4fa8645fe76c0
sojunx <phatlee1104@gmail.com>
2026-01-14 21:38:55 +0700

    added init user & cart

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,5 +1,14 @@
--- Inserting products with UUIDs for testing
--- Ensure 'category' matches your ProductCategory enum names (e.g., ELECTRONICS, CLOTHING)
+INSERT INTO users (id, email, password_hash, full_name, role, created_at, updated_at)
+VALUES ('e9609483-293e-4bc0-a4de-04d04b5d6ea3',
+        'test.user@mail.com',
+        '$2a$10$eh4SxgtEs4AgNtpYgXTn1OPpPCWn0z47RlGduHnJ.jZEOCaAZ1MJW',
+        'Nguyen Van A',
+        'USER',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP);
+
+INSERT INTO carts (id, user_id)
+VALUES (1, 'e9609483-293e-4bc0-a4de-04d04b5d6ea3');
 
 INSERT INTO products (id, sku, name, description, price, category, created_at, updated_at)
 VALUES (random_uuid(), 'SKU-001', 'Classic White T-Shirt', 'A comfortable 100% cotton white t-shirt.', 19.99,
